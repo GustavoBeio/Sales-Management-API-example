0ebe57d2fe02018ef1b3eb908113514246b1bb7c
Gustavo <gustavo.souzabeio@gmail.com>
2024-02-07 13:04:24 -0300

    altered ExistsAsync in ICLientRepository to ExistsbyIdAsync created ExistsbyNameAsync task in IClientRepository

--- Sales.Domain/Interfaces/Repositories/IClientRepository.cs
+++ Sales.Domain/Interfaces/Repositories/IClientRepository.cs
@@ -7,7 +7,8 @@ namespace Sales.Domain.Interfaces.Repositories
         Task CreateAsync(ClientModel client);
         Task UpdateAsync(ClientModel client);
         Task DeleteAsync(string clientId);
-        Task<bool> ExistsAsync(string clientId);
+        Task<bool> ExistsbyIdAsync(string clientId);
+        Task<bool> ExistsbyNameAsync(string clientName);
         Task<ClientModel> GetbyIdAsync(string clientId);
         Task<List<ClientModel>> ListbyFilterAsync(string clientId, string name);
         
