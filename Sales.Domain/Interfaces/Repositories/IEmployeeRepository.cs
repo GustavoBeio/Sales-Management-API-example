4b70ff47b8c02fa47deb66efe74a838a8176b369
Gustavo <gustavo.souzabeio@gmail.com>
2024-02-07 13:01:57 -0300

    Revert "altered ExistsAsync in IEmployeeRepository to ExistsbyIdAsync"
    This reverts commit f25185783b9cc5eaed2a06915b33a4466ba3d66a.

--- Sales.Domain/Interfaces/Repositories/IEmployeeRepository.cs
+++ Sales.Domain/Interfaces/Repositories/IEmployeeRepository.cs
@@ -7,8 +7,7 @@ namespace Sales.Domain.Interfaces.Repositories
         Task CreateAsync(EmployeeModel employee);
         Task UpdateAsync(EmployeeModel employee);
         Task DeleteAsync(string employeeId);
-        Task<bool> ExistsbyIdAsync(string employeeId);
-        Task<bool> ExistsbyLoginAsync(string employeeName);
+        Task<bool> ExistsAsync(string employeeId);
         Task<EmployeeModel> GetbyIdAsync(string employeeId);
         Task<List<EmployeeModel>> ListbyFilterAsync(string employeeId, string name);
     }
