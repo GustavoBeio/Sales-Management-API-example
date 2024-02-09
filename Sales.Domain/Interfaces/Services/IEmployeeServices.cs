25697f8e83f88013b4f5722f24f28680e1fad75b
Gustavo <gustavo.souzabeio@gmail.com>
2024-02-09 13:11:05 -0300

    cleaned unused namespaces

---
+++ Sales.Domain/Interfaces/Services/IEmployeeServices.cs
@@ -0,0 +1,14 @@
+﻿using Sales.Domain.Models;
+
+namespace Sales.Domain.Interfaces.Services
+{
+    public interface IEmployeeServices
+    {
+        Task<bool> AuthAsync(EmployeeModel employee);
+        Task CreateAsync(EmployeeModel employee);
+        Task UpdateAsync(EmployeeModel employee);
+        Task DeleteAsync(string employeeId);
+        Task<EmployeeModel> GetbyIdAsync(string employeeId);
+        Task<List<EmployeeModel>> ListbyFilterAsync(string employeeId, string name);
+    }
+}
