76ff221426449d0e9cecf02841702e41c7f85af3
Gustavo <gustavo.souzabeio@gmail.com>
2024-02-15 11:04:58 -0300

    Criado as classes de serviço do Domain da solução

---
+++ Sales.Domain/Services/ProductOrderServices.cs
@@ -0,0 +1,38 @@
+﻿using Sales.Domain.Interfaces.Services;
+using Sales.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Domain.Services
+{
+    public class ProductOrderServices : IProductOrderServices
+    {
+        public Task CreateAsync(ProductOrderModel productOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string productOrderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductOrderModel> GetbyIdAsync(string productOrderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductOrderModel>> ListbyFilterAsync(string productOrderId, string clientId, string employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdadteAsync(ProductOrderModel productOrder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
