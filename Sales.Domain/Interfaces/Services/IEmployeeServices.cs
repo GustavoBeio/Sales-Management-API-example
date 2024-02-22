5721db996e61ad2278e94454567649070c77edbc
GustavoBeio <gustavo.souzabeio@gmail.com>
2024-02-22 19:23:14 -0300

    feat: added Response as a type argument on the IEmployee interface

--- Sales.Domain/Interfaces/Services/IEmployeeServices.cs
+++ Sales.Domain/Interfaces/Services/IEmployeeServices.cs
@@ -1,14 +1,15 @@
 ﻿using Sales.Domain.Models;
+using Sales.Domain.Validations.Base;
 
 namespace Sales.Domain.Interfaces.Services
 {
     public interface IEmployeeServices
     {
-        Task<bool> AuthAsync(EmployeeModel employee);
-        Task CreateAsync(EmployeeModel employee);
-        Task UpdateAsync(EmployeeModel employee);
-        Task DeleteAsync(string employeeId);
-        Task<EmployeeModel> GetbyIdAsync(string employeeId);
-        Task<List<EmployeeModel>> ListbyFilterAsync(string employeeId, string name);
+        Task<Response<bool>> AuthAsync(EmployeeModel employee);
+        Task<Response> CreateAsync(EmployeeModel employee);
+        Task<Response> UpdateAsync(EmployeeModel employee);
+        Task<Response> DeleteAsync(string employeeId);
+        Task<Response<EmployeeModel>> GetbyIdAsync(string employeeId);
+        Task<Response<List<EmployeeModel>>> ListbyFilterAsync(string employeeId, string name);
     }
 }
