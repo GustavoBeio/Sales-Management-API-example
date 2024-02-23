fb81bdc7a32d2ae9b5d6683b95b1fc98610fb4b1
Gustavo <gustavo.souzabeio@gmail.com>
2024-02-23 17:31:28 -0300

    fix: Data is no longer readonly

--- Sales.Domain/Models/ClientModel.cs
+++ Sales.Domain/Models/ClientModel.cs
@@ -8,6 +8,7 @@ namespace Sales.Domain.Models
 {
     public class ClientModel : EntityBase
     {
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
