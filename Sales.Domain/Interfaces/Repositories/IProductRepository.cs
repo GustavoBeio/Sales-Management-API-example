81f29ce5d6b259679785d5db3c803cc4acb62e9b
GustavoBeio <gustavo.souzabeio@gmail.com>
2024-02-26 17:48:01 -0300

    feat: Implemented ProductServices

--- Sales.Domain/Interfaces/Repositories/IProductRepository.cs
+++ Sales.Domain/Interfaces/Repositories/IProductRepository.cs
@@ -7,7 +7,7 @@ namespace Sales.Domain.Interfaces.Repositories
         public Task CreateAsync(ProductModel product);
         public Task UpdateAsync(ProductModel product);
         public Task DeleteAsync(string productId);
-        public Task<bool> ExistsAsync(string productId);
+        public Task<bool> ExistsbyIdAsync(string productId);
         public Task<ProductModel> GetbyIdAsync(string productId);
         public Task<List<ProductModel>> ListbyFilterAsync(string productId, string clientId);
     }
