eb4fb619460820e60138801f09925a22f476e164
Gustavo <gustavo.souzabeio@gmail.com>
2024-02-27 17:27:27 -0300

    fix: removed Id property from ClientModel as it already inherits it from EntityBase

--- Sales.Domain/Models/ClientModel.cs
+++ Sales.Domain/Models/ClientModel.cs
@@ -8,7 +8,6 @@ namespace Sales.Domain.Models
 {
     public class ClientModel : EntityBase
     {
-        public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
